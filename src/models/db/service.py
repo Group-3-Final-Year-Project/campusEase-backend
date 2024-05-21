492338221ab6cacb8f88f0dffd46ea84bb9cef31
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2024-05-21 12:00:31 +0000

    feat(service): :sparkles: worked on service table

--- src/models/db/service.py
+++ src/models/db/service.py
@@ -1,7 +1,9 @@
 import datetime
+import json
 import sqlalchemy
 from sqlalchemy.orm import Mapped as SQLAlchemyMapped, mapped_column as sqlalchemy_mapped_column
 from sqlalchemy.sql import functions as sqlalchemy_functions
+from sqlalchemy.orm import relationship
 
 from src.repository.table import Base
 
@@ -9,7 +11,28 @@ class Service(Base):
     __tablename__ = 'service'
     
     id:SQLAlchemyMapped[str] = sqlalchemy_mapped_column(primary_key=True)
+    provider_id:SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.ForeignKey("account.id"),nullable=False)
     name: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(
-        sqlalchemy.String(length=64), nullable=False, unique=True
+        sqlalchemy.String(length=100), nullable=False, unique=True
+    )
+    description: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.Text(), nullable=True)
+    category: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.String(length=100), nullable=False)
+    cover_image: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.String(),nullable=True)
+    gallery: SQLAlchemyMapped[json.JSONEncoder] = sqlalchemy_mapped_column(sqlalchemy.JSON)
+    prices: SQLAlchemyMapped[json.JSONEncoder] = sqlalchemy_mapped_column(sqlalchemy.JSON)
+    email: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.String(),nullable=True)
+    website: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.String(),nullable=True)
+    location: SQLAlchemyMapped[str] = sqlalchemy_mapped_column(sqlalchemy.String(),nullable=True)
+    is_available: SQLAlchemyMapped[bool] = sqlalchemy_mapped_column(sqlalchemy.Boolean, nullable=False, default=True)
+    created_at: SQLAlchemyMapped[datetime.datetime] = sqlalchemy_mapped_column(
+        sqlalchemy.DateTime(timezone=True), nullable=False, server_default=sqlalchemy_functions.now()
+    )
+    updated_at: SQLAlchemyMapped[datetime.datetime] = sqlalchemy_mapped_column(
+        sqlalchemy.DateTime(timezone=True),
+        nullable=True,
+        server_onupdate=sqlalchemy.schema.FetchedValue(for_update=True),
     )
     # other attributes that we will need for service will be added here
+
+    provider = relationship("Account", backref="services")
+    __mapper_args__ = {"eager_defaults": True}
